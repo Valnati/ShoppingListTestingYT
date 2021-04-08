0867c78b7872e6cb303e3de55d7ba3ac5ebf7535
Valnati <workforthefuture@gmail.com>
2021-04-08 15:49:58 -0700

    Created fake repository to prepare for testing

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/repositories/DefaultShoppingRepository.kt
@@ -0,0 +1,52 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import com.androiddevs.shoppinglisttestingyt.ShoppingRepository
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+import retrofit2.Response
+import java.lang.Exception
+import javax.inject.Inject
+
+class DefaultShoppingRepository @Inject constructor(
+    private val shoppingDao: ShoppingDao,
+    private val pixabayAPI: PixabayAPI
+) : ShoppingRepository {
+    //multiple repositories, so take advantage of interface to define its responsibilities
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.insertShoppingItem(shoppingItem)
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.deleteShoppingItem(shoppingItem)
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return shoppingDao.observeAllShoppingItems()
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return shoppingDao.observeTotalPrice()
+    }
+
+    //we return a resource, from our other package
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return try {
+            //use the API query and check status
+            val response = pixabayAPI.searchForImage(imageQuery)
+            if(response.isSuccessful) {
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?: Resource.error("An unknown error occurred", null)
+            } else {
+                Resource.error("An unknown error occurred", null)
+            }
+        } catch (e: Exception) {
+            Resource.error("Couldn't reach server. Check internet connection", null)
+
+        }
+    }
+}
---
+++ app/src/test/java/com/androiddevs/shoppinglisttestingyt/repositories/FakeShoppingRepository.kt
@@ -0,0 +1,65 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.androiddevs.shoppinglisttestingyt.ShoppingRepository
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+
+class FakeShoppingRepository : ShoppingRepository{
+    //sole purpose is to be passed along, so it's less complicated than otherwise
+    //these objects are simulating the result of functions! That would pass the objects
+    //after doing complicated calls and such
+    //so built for testing, not for production
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+    //list is instantiated through the given item
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    //allows for the actual data to be updated on a change
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice() : Float {
+        //get sum of everything in mutable list, through doubles
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    //sends dummy data back based on error toggle
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if(shouldReturnNetworkError) {
+            Resource.error("Error", null)
+        } else {
+            Resource.success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+
+
+}
