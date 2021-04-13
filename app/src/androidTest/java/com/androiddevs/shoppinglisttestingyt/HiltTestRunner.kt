13e6f974ade7adc1a39d1971032f2e49fd085a89
Valnati <workforthefuture@gmail.com>
2021-04-12 19:19:58 -0700

    theoretically hilt now injects database into testing

---
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/HiltTestRunner.kt
@@ -0,0 +1,20 @@
+package com.androiddevs.shoppinglisttestingyt
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner : AndroidJUnitRunner() {
+
+    //required to have hilt  run through tests
+    //normally return the className based on the application
+    //we instead route through the custom Hilt playground
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
