c0ccab7f69c020b32807eb217ab39546b9f3cffb
Valnati <workforthefuture@gmail.com>
2021-04-20 17:05:35 -0700

    implemented add shopping item and relevant test

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragmentTest.kt
@@ -1,13 +1,16 @@
 package com.androiddevs.shoppinglisttestingyt.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
 import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
 import com.androiddevs.shoppinglisttestingyt.launchFragmentInHiltContainer
 import com.androiddevs.shoppinglisttestingyt.repositories.FakeShoppingRepositoryAndroidTest
@@ -20,6 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -28,6 +32,12 @@ class AddShoppingItemFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
     @Before
     fun setup() {
@@ -62,7 +72,7 @@ class AddShoppingItemFragmentTest {
 
     @Test
     fun pressBackButton_imageUrlIsEmpty() {
-        var fakeViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        val fakeViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
         val navController = mock(NavController::class.java)
 
         launchFragmentInHiltContainer<AddShoppingItemFragment> {
@@ -71,6 +81,24 @@ class AddShoppingItemFragmentTest {
         }
         pressBack()
         Truth.assertThat(fakeViewModel.curImageUrl.getOrAwaitValue()).isEqualTo("")
+    }
 
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("testName"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("800.3"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        Truth.assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("testName", 5, 800.3f, ""))
     }
 }
